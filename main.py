6e66347a86f468a15d3b11f47c4060969cf32bd7
unknown <christian_jlago@alumnos-santagema.es>
2025-10-03 19:29:46 +0200

    s

--- main.py
+++
@@ -1,6 +0,0 @@
-from PyQt5.QtWidgets import QApplication # type: ignore
-from interfaz import interfazUsuario
-aplicacion = QApplication([])
-uiUsuario = interfazUsuario()
-uiUsuario.show()
-aplicacion.exec_()
