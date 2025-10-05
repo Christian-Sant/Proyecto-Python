8e4990be9f17f9643c6ce5d6e23a9eb3e629df13
Christian-Sant <christian_jlago@alumnos-santagema.es>
2025-10-05 19:32:31 +0200

    He conectado la base de datos con el registro, lo mas seguro es que cambie la base de datos y ponga de pk el correo en vez de ID (#2)

--- baseDeDatos.py
+++ baseDeDatos.py
@@ -1,13 +1,21 @@
 import sqlite3
-
+from utilidades import idAzarUsuario
 def baseDeDatos():
     conexion = sqlite3.connect("IncidenciasInformaticas.db") 
     conexion.execute("PRAGMA foreign_keys = ON;") 
     cursor = conexion.cursor()
 
-    # Crear una tabla de ejemplo
+    cursor.execute("""
+        ID_Usuario VARCHAR(12),
+        Usuario VARCHAR(20),
+        Correo VARCHAR(100),
+        Contraseña VARCHAR(100),
+        CONSTRAINT PK_IDUSUARIO PRIMARY KEY (ID_Usuario)
+    )
+    """)
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS Incidencias (
+        ID_Usuario VARCHAR(12),
         ID_Incidencia VARCHAR(12),
         Titulo VARCHAR(40),
         Descripcion VARCHAR(200),
@@ -16,22 +24,33 @@ def baseDeDatos():
         Estado VARCHAR(7),
         CONSTRAINT PK_IDINCIDENCIAS PRIMARY KEY (ID_Incidencia),
         CONSTRAINT CHK_GRAVEDAD CHECK (Gravedad IN ('Baja', 'Media', 'Alta', 'Grave', 'Muy Grave')),
-        CONSTRAINT CHK_ESTADO CHECK (Estado IN ('ABIERTO', 'CERRADO'))
+        CONSTRAINT CHK_ESTADO CHECK (Estado IN ('ABIERTO', 'CERRADO')),
+        CONSTRAINT FK_INCIDENCIA FOREIGN KEY (ID_Usuario) REFERENCES Usuarios (ID_Usuario) ON UPDATE CASCADE
     )
     """)
+    conexion.commit()
+    conexion.close()
 
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS Usuarios (
-        ID_Usuario VARCHAR(12),
-        Usuario VARCHAR(20),
-        Correo VARCHAR(100),
-        Contraseña VARCHAR(100),
-        ID_Incidencia VARCHAR(12),
-        CONSTRAINT PK_IDUSUARIO PRIMARY KEY (ID_Usuario),
-        CONSTRAINT FK_INCIDENCIA FOREIGN KEY (ID_Incidencia)
-        REFERENCES Incidencias (ID_Incidencia)
-            ON UPDATE CASCADE
+def vistaIdUsuario():
+    conexion = sqlite3.connect("IncidenciasInformaticas.db") 
+    cursor = conexion.cursor()
+    consulta = ("Select ID_Usuario from Usuarios")
+    cursor.execute(consulta)
+    resultados = cursor.fetchall()
+    conexion.close()
+    return resultados
+
+def insertarUsuario(correo, usuario, password):
+    
+    consulta = (
+        "INSERT INTO USUARIOS (id_usuario,usuario, correo, Contraseña) VALUES ('"
+        + idAzarUsuario() + "', '"
+        + usuario + "', '"
+        + correo + "', '"
+        + password + "')"
     )
-    """)
+    conexion = sqlite3.connect("IncidenciasInformaticas.db") 
+    cursor = conexion.cursor()
+    cursor.execute(consulta)
     conexion.commit()
     conexion.close()
--- interfaz.py
+++ interfaz.py
@@ -1,6 +1,6 @@
-from PyQt5.QtWidgets import QApplication, QLineEdit, QWidget, QLabel, QPushButton # type: ignore
+from PyQt5.QtWidgets import QApplication, QLineEdit, QMessageBox, QWidget, QLabel, QPushButton # type: ignore
 from PyQt5.QtGui import QFont #type: ignore
-
+from baseDeDatos import insertarUsuario
 class interfazUsuario(QWidget) :
     def __init__(self):
         super().__init__()
@@ -10,7 +10,7 @@ class interfazUsuario(QWidget) :
         ############################################### Titulo de Inicio de Sesion
         self.txtIniciarSesion = QLabel("Iniciar Sesion", self)
         self.txtIniciarSesion.move(100, 80)
-        self.txtIniciarSesion.setFont(QFont("Arial", 12)) 
+        self.txtIniciarSesion.setFont(QFont("Arial", 12, QFont.Bold))
         ###############################################
         self.txtusuario = QLabel("Usuario: ", self)
         self.txtusuario.move(40, 118)
@@ -29,6 +29,7 @@ class interfazUsuario(QWidget) :
         self.password.move(40,190)
         self.password.resize(220, 20)
         self.password.setFont(QFont("Arial", 12))
+        self.password.setEchoMode(QLineEdit.Password)
         ###############################################
         self.iniciarSesion = QPushButton("Iniciar Sesión", self)
         self.iniciarSesion.resize(120,30)
@@ -39,3 +40,99 @@ class interfazUsuario(QWidget) :
         self.registrarse.resize(90,30)
         self.registrarse.setFont(QFont("Arial", 12))
         self.registrarse.move(40,215)
+        ##############################################
+        self.registrarse.clicked.connect(self.registrar)
+        ##############################################
+    def registrar(self):
+        ###############################################
+        self.txtIniciarSesion.hide()
+        self.usuario.hide()
+        self.password.hide()
+        self.txtusuario.hide()
+        self.txtPassword.hide()
+        self.iniciarSesion.hide()
+        self.registrarse.hide()
+        ###############################################
+        self.setWindowTitle("Registrarse")
+        self.setGeometry(200,200,300,300)
+        self.move(800,400)
+        ############################################### 
+        self.txtRegistrarse = QLabel("Registrarse", self)
+        self.txtRegistrarse.move(100, 30)
+        self.txtRegistrarse.setFont(QFont("Arial", 12, QFont.Bold))
+        self.txtRegistrarse.show() 
+        ###############################################
+        self.txtCorreo = QLabel("Correo: ", self)
+        self.txtCorreo.move(40, 60)
+        self.txtCorreo.setFont(QFont("Arial", 12)) 
+        self.txtCorreo.show()
+        ###############################################
+        self.correo= QLineEdit(self) 
+        self.correo.move(40,80)
+        self.correo.resize(220, 20)
+        self.correo.setFont(QFont("Arial", 12))
+        self.correo.show()
+        ###############################################
+        self.txtusuarioRegistro = QLabel("Usuario: ", self)
+        self.txtusuarioRegistro.move(40, 108)
+        self.txtusuarioRegistro.setFont(QFont("Arial", 12)) 
+        self.txtusuarioRegistro.show()
+        ###############################################
+        self.txtPasswordRegistro = QLabel("Contraseña: ", self)
+        self.txtPasswordRegistro.move(40, 153)
+        self.txtPasswordRegistro.setFont(QFont("Arial", 12)) 
+        self.txtPasswordRegistro.show()
+        ############################################### 
+        self.usuarioRegistro = QLineEdit(self) 
+        self.usuarioRegistro.move(40,128)
+        self.usuarioRegistro.resize(220, 20)
+        self.usuarioRegistro.setFont(QFont("Arial", 12))
+        self.usuarioRegistro.show()
+        ###############################################
+        self.passwordRegistro = QLineEdit(self)
+        self.passwordRegistro.move(40,173)
+        self.passwordRegistro.resize(220, 20)
+        self.passwordRegistro.setFont(QFont("Arial", 12))
+        self.passwordRegistro.setEchoMode(QLineEdit.Password)
+        self.passwordRegistro.show()
+        ###############################################
+        self.registrarse2 = QPushButton("Registrarse", self)
+        self.registrarse2.resize(100,30)
+        self.registrarse2.setFont(QFont("Arial", 12))
+        self.registrarse2.move(100,255)
+        self.registrarse2.show()
+        ##############################################
+        self.txtConfirmarPasswordRegistro = QLabel("Repite la Contraseña: ", self)
+        self.txtConfirmarPasswordRegistro.move(40, 200)
+        self.txtConfirmarPasswordRegistro.setFont(QFont("Arial", 12)) 
+        self.txtConfirmarPasswordRegistro.show()
+        ##############################################
+        self.confirmarPasswordRegistro = QLineEdit(self) 
+        self.confirmarPasswordRegistro.move(40,220)
+        self.confirmarPasswordRegistro.resize(220, 20)
+        self.confirmarPasswordRegistro.setFont(QFont("Arial", 12))
+        self.confirmarPasswordRegistro.setEchoMode(QLineEdit.Password)
+        self.confirmarPasswordRegistro.show()
+        self.registrarse2.clicked.connect(self.validarYCrearUsuario)
+        ##############################################
+    def crearUsuario(self):
+        correo = self.correo.text()
+        usuario = self.usuarioRegistro.text()
+        password = self.passwordRegistro.text()
+        insertarUsuario(correo, usuario, password)
+        
+    def validarYCrearUsuario(self):
+        passwordVerificar = self.passwordRegistro.text()
+        passwordConfirmar = self.confirmarPasswordRegistro.text()
+        
+        if passwordVerificar == passwordConfirmar:
+            self.crearUsuario()
+        else:
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Warning)
+            msg.setWindowTitle("Error")
+            msg.setText("Las contraseñas no coinciden")
+            msg.exec_()
+
+       
+        
