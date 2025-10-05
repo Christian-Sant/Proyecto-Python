ad49e1cdf21ecb0af7727342392284d1f39a69b4
Christian-Sant <christian_jlago@alumnos-santagema.es>
2025-10-05 20:24:35 +0200

    Christian (#4)
    * He conectado la base de datos con el registro, lo mas seguro es que cambie la base de datos y ponga de pk el correo en vez de ID
    
    * lo del registro ya esta todo, falta la parte de inicio

--- interfaz.py
+++ interfaz.py
@@ -12,18 +12,18 @@ class interfazUsuario(QWidget) :
         self.txtIniciarSesion.move(100, 80)
         self.txtIniciarSesion.setFont(QFont("Arial", 12, QFont.Bold))
         ###############################################
-        self.txtusuario = QLabel("Usuario: ", self)
-        self.txtusuario.move(40, 118)
-        self.txtusuario.setFont(QFont("Arial", 12)) 
+        self.txtCorreoIS = QLabel("Correo: ", self)
+        self.txtCorreoIS.move(40, 118)
+        self.txtCorreoIS.setFont(QFont("Arial", 12)) 
         ###############################################
         self.txtPassword = QLabel("Contraseña: ", self)
         self.txtPassword.move(40, 173)
         self.txtPassword.setFont(QFont("Arial", 12)) 
         ############################################### 
-        self.usuario = QLineEdit(self) 
-        self.usuario.move(40,135)
-        self.usuario.resize(220, 20)
-        self.usuario.setFont(QFont("Arial", 12))
+        self.CorreoIS = QLineEdit(self) 
+        self.CorreoIS.move(40,135)
+        self.CorreoIS.resize(220, 20)
+        self.CorreoIS.setFont(QFont("Arial", 12))
         ###############################################
         self.password = QLineEdit(self)
         self.password.move(40,190)
@@ -46,9 +46,9 @@ class interfazUsuario(QWidget) :
     def registrar(self):
         ###############################################
         self.txtIniciarSesion.hide()
-        self.usuario.hide()
+        self.CorreoIS.hide()
         self.password.hide()
-        self.txtusuario.hide()
+        self.txtCorreoIS.hide()
         self.txtPassword.hide()
         self.iniciarSesion.hide()
         self.registrarse.hide()
@@ -67,30 +67,19 @@ class interfazUsuario(QWidget) :
         self.txtCorreo.setFont(QFont("Arial", 12)) 
         self.txtCorreo.show()
         ###############################################
-        self.correo= QLineEdit(self) 
-        self.correo.move(40,80)
-        self.correo.resize(220, 20)
-        self.correo.setFont(QFont("Arial", 12))
-        self.correo.show()
-        ###############################################
-        self.txtusuarioRegistro = QLabel("Usuario: ", self)
-        self.txtusuarioRegistro.move(40, 108)
-        self.txtusuarioRegistro.setFont(QFont("Arial", 12)) 
-        self.txtusuarioRegistro.show()
+        self.correoRegistro = QLineEdit(self) 
+        self.correoRegistro.move(40,80)
+        self.correoRegistro.resize(220, 20)
+        self.correoRegistro.setFont(QFont("Arial", 12))
+        self.correoRegistro.show()
         ###############################################
         self.txtPasswordRegistro = QLabel("Contraseña: ", self)
-        self.txtPasswordRegistro.move(40, 153)
+        self.txtPasswordRegistro.move(40, 120)
         self.txtPasswordRegistro.setFont(QFont("Arial", 12)) 
         self.txtPasswordRegistro.show()
-        ############################################### 
-        self.usuarioRegistro = QLineEdit(self) 
-        self.usuarioRegistro.move(40,128)
-        self.usuarioRegistro.resize(220, 20)
-        self.usuarioRegistro.setFont(QFont("Arial", 12))
-        self.usuarioRegistro.show()
         ###############################################
         self.passwordRegistro = QLineEdit(self)
-        self.passwordRegistro.move(40,173)
+        self.passwordRegistro.move(40,140)
         self.passwordRegistro.resize(220, 20)
         self.passwordRegistro.setFont(QFont("Arial", 12))
         self.passwordRegistro.setEchoMode(QLineEdit.Password)
@@ -103,36 +92,45 @@ class interfazUsuario(QWidget) :
         self.registrarse2.show()
         ##############################################
         self.txtConfirmarPasswordRegistro = QLabel("Repite la Contraseña: ", self)
-        self.txtConfirmarPasswordRegistro.move(40, 200)
+        self.txtConfirmarPasswordRegistro.move(40, 173)
         self.txtConfirmarPasswordRegistro.setFont(QFont("Arial", 12)) 
         self.txtConfirmarPasswordRegistro.show()
         ##############################################
         self.confirmarPasswordRegistro = QLineEdit(self) 
-        self.confirmarPasswordRegistro.move(40,220)
+        self.confirmarPasswordRegistro.move(40,193)
         self.confirmarPasswordRegistro.resize(220, 20)
         self.confirmarPasswordRegistro.setFont(QFont("Arial", 12))
         self.confirmarPasswordRegistro.setEchoMode(QLineEdit.Password)
         self.confirmarPasswordRegistro.show()
-        self.registrarse2.clicked.connect(self.validarYCrearUsuario)
+        self.registrarse2.clicked.connect(self.verificarCorreo)
         ##############################################
     def crearUsuario(self):
-        correo = self.correo.text()
-        usuario = self.usuarioRegistro.text()
+        correo = self.correoRegistro.text()
         password = self.passwordRegistro.text()
-        insertarUsuario(correo, usuario, password)
+        insertarUsuario(correo, password)
         
     def validarYCrearUsuario(self):
         passwordVerificar = self.passwordRegistro.text()
         passwordConfirmar = self.confirmarPasswordRegistro.text()
-        
-        if passwordVerificar == passwordConfirmar:
+        if passwordVerificar.strip() != "" and passwordVerificar == passwordConfirmar:
             self.crearUsuario()
         else:
-            msg = QMessageBox()
-            msg.setIcon(QMessageBox.Warning)
-            msg.setWindowTitle("Error")
-            msg.setText("Las contraseñas no coinciden")
-            msg.exec_()
-
-       
-        
+            mensajeErrorPassword = QMessageBox()
+            mensajeErrorPassword.setIcon(QMessageBox.Warning)
+            mensajeErrorPassword.setWindowTitle("Error")
+            mensajeErrorPassword.setText("Las contraseñas no coinciden")
+            mensajeErrorPassword.exec_()
+    def verificarCorreo(self):
+        from baseDeDatos import vistaCorreo
+        correos = vistaCorreo()
+        pkCorreos = []
+        for i in correos:
+            pkCorreos.append(i[0])
+        if self.correoRegistro.text() in pkCorreos:
+            mensajeErrorCorreo = QMessageBox()
+            mensajeErrorCorreo.setIcon(QMessageBox.Warning)
+            mensajeErrorCorreo.setWindowTitle("Error")
+            mensajeErrorCorreo.setText("El correo ya esta registrado")
+            mensajeErrorCorreo.exec_()
+        else:
+            self.validarYCrearUsuario()
--- utilidades.py
+++ utilidades.py
@@ -1,18 +1,18 @@
 import random
 import string
-def idAzarUsuario():
-    from baseDeDatos import vistaIdUsuario
-    ids = vistaIdUsuario()
-    idsUsuarios = []
-    for i in ids:
-        idsUsuarios.append(i[0])
+#def idAzarUsuario(): Voy a USAR ESTO DESPUES PARA LA PARTE DE INCIDENCIAS.
+    #from baseDeDatos import vistaIdUsuario
+    #ids = vistaIdUsuario()
+    #idsUsuarios = []
+    #for i in ids:
+    #   idsUsuarios.append(i[0])
 
-    caracteres = string.ascii_letters + string.digits
-    while True:
-        id_usuario = ""
-        for i in range(12):
-            id_usuario = id_usuario + random.choice(caracteres)
-        if id_usuario in idsUsuarios:
-            continue
-        else: 
-            return id_usuario
+    #caracteres = string.ascii_letters + string.digits
+    #while True:
+    #    id_usuario = ""
+    #   for i in range(12):
+    #       id_usuario = id_usuario + random.choice(caracteres)
+    #   if id_usuario in idsUsuarios:
+    #       continue
+    #   else: 
+    #       return id_usuario
